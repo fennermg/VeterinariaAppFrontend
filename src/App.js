c584b8b68fda203bceabd9174f0bffe6189ec1c6
Obed Herrera <herreraobed0272@gmail.com>
2023-03-23 22:22:39 -0600

    Cambio para probar el Dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Dashboard from "./Components/Dashboard/Dashboard";
 import Login from "./Components/Login/Login";
 
 function App(){
   return(
     <div className = "flex w-full h-screen">
       <div className = "w-full flex items-center justify-center">
-        <Login/>
+        <Dashboard/>
       </div>
     </div>
   );
