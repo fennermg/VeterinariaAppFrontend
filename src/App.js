fda3c88078196ef7894aa7070689c023d9f8bafe
Obed Herrera <herreraobed0272@gmail.com>
2023-03-26 16:55:14 -0600

    Router agregado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Login from "./Components/Login/Login";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App(){
   return(
     <div className = "flex w-full h-screen">
       <div className = "w-full flex items-center justify-center">
-        <Dashboard/>
+        <Router>
+          <Routes>
+            <Route path = "/" element = {<Login/>}/>
+            <Route path = "/Dashboard" element = {<Dashboard/>}/>
+          </Routes>
+        </Router>
       </div>
     </div>
   );
